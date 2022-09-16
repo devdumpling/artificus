5b0283c1543037eba61ab6c2dc1422299e938c07
Devon Wells <devon.wellsa@gmail.com>
2022-09-16 11:59:21 -0400

    init: changesets

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# artificus
+
+## 1.0.1
+
+### Patch Changes
+
+- Introduce changesets and revert do some app cleanup
+- Updated dependencies
+  - artificus@1.0.1
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import 'nextra-theme-docs/style.css';
 
 export default function Nextra({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return <Component {...pageProps} />;
 }
